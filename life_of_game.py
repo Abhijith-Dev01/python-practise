ae300d4f57e0754c6191a91a92b67bfc4f927eef
abhijith <abhis3337@gmail.com>
2021-10-01 14:56:00 +0530

    life of game program success

--- life_of_game.py
+++ life_of_game.py
@@ -1,3 +1,4 @@
+from typing import List
 import numpy as np
 def draw(arr,m,n):
  for  i in range(m):
@@ -18,39 +19,89 @@ def rules(cell,neighbours):
     
     return cell
 
-def no_of_neighbour(row,col,tot_row,tot_col):
-    if (row == 0) or (row == tot_row -1):
-        if (col == 0) or (col == tot_col-1):
-            return 3
+
+def neighbour_indent(row,col,tot_row,tot_col):#get the neighbour indentations
+    if row == 0:
+        if col == 0:
+            lst = list(([row+1,col],[row+1,col+1],[row,col+1]))
+            return lst
+
+        if col == tot_col-1:
+            lst = list(([row+1,col],[row+1,col-1],[row,col-1]))
+            return lst
         else:
-            return 5
-    elif (col == 0) or (col == tot_col-1):
-        if (row == 0) or (row == tot_row-1):
-            return 3
+            lst = list(([row,col+1],[row+1,col+1],
+                             [row+1,col],[row+1,col-1],[row,col-1]))
+            return lst
+
+    if col  == 0:
+        if row == tot_row-1:
+            lst = list(([row-1,col],[row-1,col+1],[row,col+1]))
+            return lst
+        else:    
+            lst = list(([row-1,col],[row+1,col],[row+1,col+1],
+                             [row-1,col+1],[row,col+1]))
+            return lst
+
+    if row == tot_row-1:
+        if col == tot_col-1:
+            lst = list(([row-1,col],[row-1,col-1],[row,col-1]))
+            return lst
         else:
-            return 5
+            lst = list(([row-1,col],[row,col-1],[row,col+1],
+                             [row-1,col+1],[row-1,col-1]))
+            return lst
+
+    if col == tot_col-1:
+        lst = list(([row-1,col],[row+1,col],[row+1,col-1],
+                         [row-1,col-1],[row,col-1]))
+        return lst
+    
     else:
-        return 8 
+        lst = list(([row-1,col],[row-1,col-1],[row-1,col+1],
+                         [row+1,col-1],[row+1,col],[row+1,col+1],
+                         [row,col-1],[row,col+1]))
+        
+        return lst
 
-
-
-
+def no_of_neighbour(lst,arr):
+    count = 0
+    for i in lst:
+        a = i[0]
+        b = i[1]
+        if arr[a][b] == '*':
+            count = count+1
+        else:
+            continue
+    
+    return count
+ 
+    
 def main():
     # - indicates dead cells
     # * indicatees  live cells
-    m    = int(input("enter the number of rows"))
-    n    = int(input("enter the number of columns"))
-    arr  = np.full((m,n),'*')
-    arr1 = np.zeros((m,n))
-    draw(arr,m,n)
-    for  i in range(m):
-        for  j in range(n):
-            num        = no_of_neighbour(i,j,m,n)
+    arr  = [['-','-','-','-','-'],
+            ['-','-','*','-','-'],
+            ['-','-','*','-','-'],
+            ['-','-','*','-','-'],
+            ['-','-','-','-','-']]
+    row    = len(arr)
+    col    = len(arr[0])
+    arr1 = np.zeros((row,col))
+    lst1 = []
+    draw(arr,row,col)
+    for  i in range(row):
+        for  j in range(col):
+            lst1       = neighbour_indent(i,j,row,col)
+            num        = no_of_neighbour(lst1,arr)
             arr1[i][j] = num
-            arr[i][j]  =  rules(arr[i][j],num)
-        
+            # arr[i][j]  =  rules(arr[i][j],num)
+    for i in range (row):
+        for j in range(col):
+            arr[i][j] = rules(arr[i][j],arr1[i][j])
+
     print("\n")
-    draw(arr,m,n)
+    draw(arr,row,col)
 
 if __name__ == "__main__":
     main()
