66c43792d7947fe1585966f4bd1b8bc566d6ce6c
abhijith <abhis3337@gmail.com>
2021-10-04 16:10:00 +0530

    doc string added for each function

--- life_of_game.py
+++ life_of_game.py
@@ -1,11 +1,14 @@
-def draw_board(board_arr,m,n):
- for  i in range(m):
-    for j in range (n):        
+def draw_board(board_arr,row,col):
+ """A  function to print the generation board"""
+ for  i in range(row):
+    for j in range (col):        
          print(board_arr[i][j],end=' ')
     print('')
     
 
 def apply_rules(cell,neighbours):
+    """A function to apply game of life rules to the 
+       corresponding cells"""
     if cell == "*":
         if neighbours in [2,3] :
             cell = '*'
@@ -18,7 +21,8 @@ def apply_rules(cell,neighbours):
     return cell
 
 
-def neighbour_index(row,col,tot_row,tot_col):#get the neighbours index
+def neighbour_index(row,col,tot_row,tot_col):
+    """A function to return the neighbours index of the given cell"""
     if row == 0:
         if col == 0:
             index_lst = list(([row+1,col],[row+1,col+1],[row,col+1]))
@@ -63,6 +67,8 @@ def neighbour_index(row,col,tot_row,tot_col):#get the neighbours index
 
 
 def no_of_neighbour(index_lst,board_arr):
+    """"A function to return the number of neighbours of 
+        the given generation board and neighbour index list"""
     count = 0
     for i in index_lst:
         a = i[0]
@@ -74,6 +80,8 @@ def no_of_neighbour(index_lst,board_arr):
  
 
 def next_generation(row,col,gen_arr,neighbour_count_arr):
+        """A function to print the nexrt generation using 
+          the previous generation data"""
         for  i in range(row):
             for  j in range(col):
                 neighbour_lst = neighbour_index(i,j,row,col)
@@ -97,8 +105,9 @@ def main():
                   ['-','-','*','-','-'],
                   ['-','-','*','-','-'],
                   ['-','-','-','-','-']]
-    row    = len(first_gen)
-    col    = len(first_gen[0])
+    
+    row = len(first_gen)
+    col = len(first_gen[0])
 
     neighbour_count_arr = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],
                            [0,0,0,0,0],[0,0,0,0,0]]
